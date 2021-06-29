c5e7e06ca21773b8581b938aae76aa00af3653fc
Kristian Correia <kristiancorreia@hotmail.com>
2021-06-29 00:09:56 +0000

    add service account creation to iambonding unit test

---
+++ modules/service-account/output.tf
@@ -0,0 +1,3 @@
+output "service_account_name" {
+  value = google_service_account.service_account.name
+}
--- tests/unit/iam-bonding/main.tf
+++ tests/unit/iam-bonding/main.tf
@@ -2,11 +2,16 @@ provider "google" {
   project = var.project_id
 }
 
-
+module "service-account-test" {
+  source = "../../../modules/service-account"
+  project_id = var.project_id
+  account_id = "foo"
+  name = "foo"
+}
 
 module "iam-bonding-test" {
   source = "../../../modules/iam-bonding"
   project_id = var.project_id
-  member = "${var.project}@cloudbuild.gserviceaccount.com"
+  member = module.service-account-test.service_account_name
   role = "role/viewer"
 }
