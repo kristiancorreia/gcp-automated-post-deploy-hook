d9fa0bb063dedba191a5963ae8af88082076f44f
Kristian Correia <kristiancorreia@hotmail.com>
2021-06-29 22:47:42 +0000

    update typo on moduel directory

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ provider "google" {
 }
 
 module "web-hook address" {
-  source = "modules/secrets"
+  source = "./modules/secrets"
   project_id = var.project_id
   secret_name = "web-hook address"
   secret_content = var.web_hook_URL
 }
 
 module "Give GCE SA - Secret Manager Secret Accessor" {
-  source = "modules/iam-bonding"
+  source = "./modules/iam-bonding"
   project_id = var.project_id
   role = "roles/secretmanager.secretAccessor"
   member = "${project_number}-compute@developer.gserviceaccount.com"
